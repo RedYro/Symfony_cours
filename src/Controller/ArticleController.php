50c25e0a814b2f38bd1406617471031a356903b1
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-07-18 15:19:50 +0200

    Blog Projet

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,23 +3,26 @@
 namespace App\Controller;
 
 use App\Repository\ArticlesRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoriesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
     // "findById" method 
 
     #[Route('/article/{id}', name: 'app_article')]
-    public function index(ArticlesRepository $articlesRepository, int $id): Response
+    public function index(ArticlesRepository $articlesRepository, CategoriesRepository $categoriesRepository, int $id): Response
     {
         $articleId = $articlesRepository->findOneById($id);
+        $categories = $categoriesRepository->findAll();
         if(!$articleId){
             return $this->redirectToRoute('app_home');
         }
         return $this->render('article/index.html.twig', [
             'articleInfo' => $articleId,
+            'categories' => $categories
         ]);
     }
 
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArticlesRepository;
+use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +14,16 @@ class HomeController extends AbstractController
     // À ce niveau et avec cette action, il faut récupérer les articles et les stocker dans une variable
     // Afin de récupérer les données des articles, on utilise le "repository" lié à l'entité en question
     // Cela s'appelle le mécaniqme d'injection de dépendance en Symfony => il s'agit de dire à Symfony que l'on veut rentrer dans cette action en embarquant avec l'objet "ArticlesRepository" qui est une instance stocké dans "$articlesRepository"
-    public function index(ArticlesRepository $articlesRepository): Response
+    public function index(ArticlesRepository $articlesRepository, CategoriesRepository $categoriesRepository): Response
     {
         // On se sert de la variable "$articlesRepository" pour utiliser les méthodes et interroger la DB
         $articles = $articlesRepository->findAll(); // Array contenant tous les articles
         // dd($articles); // "dd()" (don't die) => fonction similaire à "debug()"
+        // Récupération des catégories 
+        $categories = $categoriesRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'articles' => $articles
+            'articles' => $articles,
+            'categories' => $categories
         ]);
     }
 }
