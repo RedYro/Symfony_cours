e5c6a9dcf57714ab01dcf52cd265644ec23be3cd
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-07-18 13:01:25 +0200

    Blog Projet

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    #[Route('/article/{id}', name: 'app_article')]
+    public function index(ArticlesRepository $articlesRepository, int $id): Response
     {
+        $articleId = $articlesRepository->findById($id);
+        // $articleInfo = $articlesRepository->findAll();
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'article' => $articleId,
+            // 'articleInfo' => $articleInfo
         ]);
     }
 }
