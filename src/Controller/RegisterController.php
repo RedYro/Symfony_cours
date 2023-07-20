c534f7e838b4e763d369bb1272d7ab79810cfe94
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-07-20 12:32:42 +0200

    Blog Projet

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Form\RegisterType;
 use App\Repository\CategoriesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +12,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class RegisterController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function index(CategoriesRepository $categoriesRepository): Response
-    {
+    public function index(CategoriesRepository $categoriesRepository, RegisterType $registerType): Response
+    {   
+        // Création d'un objet instance de la classe "Users"
+        $user = new Users;
+        // Création d'un formulaire à l'aide de la méthode "createForm()" le classe "AbstractController" 
+        $form = $this->createForm(RegisterType::class, $user); // 2 paramètres pour la méthode "createForm()" // 1 - classe du formulaire // 2 - objet géré par le formulaire
+
+        // "$categories" pour la navbar
         $categories = $categoriesRepository->findAll();
         return $this->render('register/index.html.twig', [
-            'categories' => $categories
+            'categories' => $categories,
+            'formInscription' => $form->createView() // Passage du formulaire en variable 'template' et création de la 'vue' de ce formulaire
         ]);
     }
 }
